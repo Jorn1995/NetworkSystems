55ab80331b749038e67d9d70fbb901af6b4d9df4
Daniel Sonck <daniel@sonck.nl>
2017-04-07 09:13:11 +0200

    Qt Start

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "ChatWidget.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    ChatWidget w;
+    w.show();
+
+    return a.exec();
+}
